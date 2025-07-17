45fda371b33ca40ee7fde33b1e393ae4d5ae69a0
Muhammed <lawalmuhammedope@gmail.com>
2025-07-17 23:13:07 +0100

    prod: update redis config for production

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -3,12 +3,26 @@ import { createClient } from "redis";
 
 export let prisma: PrismaClient = new PrismaClient()
 
-export const redisClient = createClient({
-    url: process.env.REDIS_URL
-});
+export let redisClient: any;
+
+if (process.env.ENV === 'local') {
+    redisClient = createClient({
+        url: process.env.REDIS_URL
+    });
+} else {
+    redisClient = createClient({
+        username: process.env.REDIS_USERNAME,
+        password: process.env.REDIS_PASSWORD,
+        socket: {
+            host: process.env.REDIS_HOST || 'localhost',
+            port: process.env.REDIS_PORT ? Number(process.env.REDIS_PORT) : 6379
+        }
+    });
+}
+
 
 export const initServices = async () => {
-    redisClient.on('error', (err) => {
+    redisClient.on('error', (err: any) => {
         console.error('Redis Client Error', err);
     }
     );
